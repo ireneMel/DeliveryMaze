0491ad168536f3c0a952c24aecab5a6abb1aa7ae
Zhelizniak Anna <anna.zhelizniak@ukma.edu.ua>
2021-05-20 11:53:42 +0300

    Try animation hero

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -9,13 +9,25 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.*;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
 import java.io.IOException;
 
 public class Main extends Application {
+
+    private HashMap<KeyCode, Boolean> keys = new HashMap<>();
+    public static ArrayList<Rectangle> bonuses = new ArrayList<>();
+    FileInputStream inputstream = new FileInputStream("src/image_2021-05-20_11-20-44-removebg-preview (1).png");
+    Image image = new Image(inputstream);
+    ImageView imageView = new ImageView(image);
+    Hero player = new Hero(imageView);
+
+    public Main() throws FileNotFoundException {
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
@@ -48,12 +60,93 @@ public class Main extends Application {
     }
 
     private void firstLevel(Stage stage) throws IOException {
+
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        //p.getChildren();
-        stage.setScene(new Scene(root, 1200,700));
+        Scene scene = new Scene(root,1200,700);
+        Pane p=(Pane) scene.lookup("#mazePane");
+        player.setMinSize(100,100);
+        player.setLayoutX(500);player.setLayoutY(500);
+        p.getChildren().addAll(player);
+/*        scene.setOnKeyPressed(event->keys.put(event.getCode(),true));
+        scene.setOnKeyReleased(event-> {
+            keys.put(event.getCode(), false);
+        });*/
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                scene.setOnKeyPressed(event->keys.put(event.getCode(),true));
+                scene.setOnKeyReleased(event-> {
+                    keys.put(event.getCode(), false);
+                });
+                if (isPressed(KeyCode.UP)) {
+                    player.animation.play();
+                    player.animation.setOffsetY(155);
+                    player.moveY(-50);
+                } else if (isPressed(KeyCode.DOWN)) {
+                    player.animation.play();
+                    player.animation.setOffsetY(0);
+                    player.moveY(2);
+                } else if (isPressed(KeyCode.RIGHT)) {
+                    player.animation.play();
+                    player.animation.setOffsetY(64);
+                    player.moveX(2);
+                } else if (isPressed(KeyCode.LEFT)) {
+                    player.animation.play();
+                    player.animation.setOffsetY(32);
+                    player.moveX(-2);
+                }
+        else{
+            player.animation.stop();
+        }
+                /* bonus();*/
+            }
+        };
+        timer.start();
+        stage.setTitle("Game");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+/*    public void bonus(){
+        int random = (int)Math.floor(Math.random()*100);
+        int x = (int)Math.floor(Math.random()*600);
+        int y = (int)Math.floor(Math.random()*600);
+        if(random == 5){
+            Rectangle rect = new Rectangle(20,20,Color.RED);
+            rect.setX(x);
+            rect.setY(y);
+            bonuses.add(rect);
+            root.getChildren().addAll(rect);
+        }
+    }*/
+    public void update() {
+        if (isPressed(KeyCode.UP)) {
+            player.animation.play();
+            player.animation.setOffsetY(155);
+            player.moveY(-50);
+        } else if (isPressed(KeyCode.DOWN)) {
+            player.animation.play();
+            player.animation.setOffsetY(0);
+            player.moveY(2);
+        } else if (isPressed(KeyCode.RIGHT)) {
+            player.animation.play();
+            player.animation.setOffsetY(64);
+            player.moveX(2);
+        } else if (isPressed(KeyCode.LEFT)) {
+            player.animation.play();
+            player.animation.setOffsetY(32);
+            player.moveX(-2);
+        }
+/*        else{
+            player.animation.stop();
+        }*/
+    }
+    public boolean isPressed(KeyCode key) {
+        return keys.getOrDefault(key, false);
     }
 
     public static void main(String[] args) {
         launch(args);
     }
+
 }
